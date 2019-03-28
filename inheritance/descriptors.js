d801ac29a6fbc4cd4187a445437def29fa01ad69
Paul Borawski <paulborawski14@gmail.com>
2019-03-28 13:08:50 -0700

    configurable: false will make Object non deletable

--- inheritance/descriptors.js
+++ inheritance/descriptors.js
@@ -1,10 +1,12 @@
 let person = { name: 'Paul', age: 31 };
 Object.defineProperty(person, 'name', {
   writable: false,
-  enumerable: false
+  enumerable: false,
+  configurable: false
 });
 
 person.name = 'Jon';
 person.age = 51;
+delete person.name;
 
 console.log(Object.keys(person))
